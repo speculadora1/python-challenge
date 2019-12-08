1a45a1c73dd11b9e31b701bde3f126898c90338d
speculadora <spencerharrison0@gmail.com>
2019-12-08 09:05:49 -0500

    Small changes to value calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,32 +17,34 @@ with open(filepath, newline = '') as csvreader:
     #    print(row)
 
     # Initialize variables
-    numMonths = 0
-    profitLoss = 0
-    lastPL = 0
-    diff = 0
-    plChanges = []
-    hiChange = 0
-    hiMonth = ""
-    loChange = 0
-    loMonth = ""
+    iterator = 1 # tracks loop iteration
+    numMonths = 0 # total number of months
+    profitLoss = 0 # total profit/loss
+    lastPL = 0 # stores previous profit/loss
+    diff = 0 # difference in profit/loss from previous month
+    plChanges = [] # list of changes in profit/loss
+    hiChange = 0 # high change in profit/loss
+    hiMonth = "" # month-year of high change
+    loChange = 0 # low change in profit/loss
+    loMonth = "" # month-year of low change
     
     # Collect data for financial analysis summary
     for row in budgetData:
         numMonths = numMonths + 1
         profitLoss = profitLoss + int(row[1])
         diff = int(row[1]) - lastPL
-        if lastPL != 0:
+        if iterator > 1:
             plChanges.append(diff)
         if int(row[1]) > hiChange:
-            hiMonth = row[0]
-            if lastPL != 0:
+            if iterator > 1:
                 hiChange = int(row[1]) - lastPL
+                hiMonth = row[0]
         if int(row[1]) < loChange:
-            loMonth = row[0]
-            if lastPL != 0:
+            if iterator > 1:
                 loChange = int(row[1]) - lastPL
+                loMonth = row[0]
         lastPL = int(row[1])
+        iterator = iterator + 1
 
 # Print summary output
 print(f"Financial Analysis\n\
