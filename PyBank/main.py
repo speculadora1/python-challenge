99cc2b10d139d7691724befd9643c8139b74aa81
speculadora <spencerharrison0@gmail.com>
2019-12-08 08:58:46 -0500

    Completed financial analysis code

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,20 +12,43 @@ with open(filepath, newline = '') as csvreader:
     header = next(budgetData)
     #print(f"CSV Header: {header}")
     
-    # Preview data set (uncommented as needed)
+    # Preview data set
     #for row in budgetData:
     #    print(row)
 
     # Initialize variables
     numMonths = 0
     profitLoss = 0
-    # Count number of rows (months) in data set
+    lastPL = 0
+    diff = 0
+    plChanges = []
+    hiChange = 0
+    hiMonth = ""
+    loChange = 0
+    loMonth = ""
+    
+    # Collect data for financial analysis summary
     for row in budgetData:
         numMonths = numMonths + 1
         profitLoss = profitLoss + int(row[1])
+        diff = int(row[1]) - lastPL
+        if lastPL != 0:
+            plChanges.append(diff)
+        if int(row[1]) > hiChange:
+            hiMonth = row[0]
+            if lastPL != 0:
+                hiChange = int(row[1]) - lastPL
+        if int(row[1]) < loChange:
+            loMonth = row[0]
+            if lastPL != 0:
+                loChange = int(row[1]) - lastPL
+        lastPL = int(row[1])
 
 # Print summary output
 print(f"Financial Analysis\n\
 ----------------------------\n\
 Total Months: {numMonths}\n\
-Total: ${profitLoss}")
+Total: ${profitLoss}\n\
+Average Change: ${round(sum(plChanges)/len(plChanges),2)}\n\
+Greatest Increase in Profits: {hiMonth} (${hiChange})\n\
+Greatest Decrease in Profits: {loMonth} (${loChange})")
